617a4dcfd2ddc98604b46510b0e38d33d83f9b6c
Jayesh Patil <jp95849@gmail.com>
2020-06-03 17:23:50 +0530

    Create TwoCityScheduling.java

---
+++ Week 1 June 1st–June 7th/TwoCityScheduling.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        //Sort
+        //Time : O(nlogn) Space: O(1)
+        //Arrays.sort(costs, Comparator.comparingInt(o -> o[0] - o[1]));
+        Arrays.sort(costs, (a, b) -> a[0] - a[1] - (b[0] - b[1]));
+        int sum = 0;
+        for(int i = 0; i < costs.length; i++){
+            if(i < costs.length / 2){
+                sum += costs[i][0];
+            }
+            else{
+                sum += costs[i][1];
+            }
+        }
+        return sum;
+    }
+}
